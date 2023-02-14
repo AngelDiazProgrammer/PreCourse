6e72318a4cd7768e2317459b28e3307d797963ec
Angel Diaz <angel.diaz@acsi.com>
2023-02-14 10:27:31 -0500

    prueba2

---
+++ inicio.js
@@ -0,0 +1,112 @@
+// Asi se comenta
+/*Asi tambien*/
+
+
+/* Tipos de datos
+undefinied, null, boolean, string, symbol, numer, object
+*/
+//String
+var nombre = "Angel";
+var apellido = "Diaz";
+console.log(nombre);
+
+//Numericas
+var num1 = 10;
+var num2 = 13;
+
+//SUMA
+var suma = nombre + " " +apellido;
+console.log(suma)
+
+//RESTA
+var resta = num1 - num2;
+console.log(resta);
+
+//MULTIPLICACION    
+var multi = num1 * num2;
+console.log(multi);
+
+//DIVISION
+var divi = num1 / num2;
+console.log(divi);
+
+//DECIMALES
+var peso = 23.40;
+
+//RESTO
+var resto = num2 % num1;
+console.log(resto);
+
+//INCREMENTAR EL VALOR DE UNA VARIABLE
+var pollos = 10;
+console.log(pollos)
+
+//opcion 1
+pollos = pollos + 1;
+console.log(pollos);
+
+//opcion 2
+pollos++;
+console.log(pollos);
+
+//DISMINUIR EL VALOR DE UNA VARIABLE
+var galletas = 300;
+console.log(galletas);
+
+//opcion1
+galletas = galletas -1;
+console.log(galletas);
+
+//opcion 2
+galletas --;
+console.log(galletas);
+
+//CAMBIAR VALOR SUMA
+var dolares = 23;
+
+//opcion1
+dolares = dolares + 5;
+console.log(dolares);
+
+//opcion2
+dolares += 5;
+console.log(dolares);
+ 
+//CAMBIAR VALOR RESTA
+var dolares = 23;
+
+//opcion1
+dolares = dolares - 5;
+console.log(dolares);
+
+//opcion2 (ejemplo deuda)
+dolares -= 5;
+console.log(dolares);
+
+//Mu8ltiplicacion
+ dolares = 10;
+ dolares *= 5;
+console.log(dolares);
+
+//Division
+dolares = 10;
+dolares /= 5;
+console.log(dolares);
+
+//CADENAS DE CARACTERES
+//los carcateres pierden sus especificacione dentro de una cadena de caracteres
+nombre= "Angel_12345678"; 
+
+nombre = "angel david del 'SENA'";
+nombre = 'angel david del "SENA"';
+
+
+//SALTO DE LINES
+nombre= "Angel \nDiaz"
+console.log(nombre);
+
+//UNIR CADENAS DE CARCATERES
+nombre= "Angel"+" David"+" Diaz"+" Flechas";
+profesion= "PROGRAMADOR"
+console.log(nombre+" "+profesion);
+
